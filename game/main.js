e787713b71af98b310b5d15544e3f32bf1f3e47d
Imjuve <beslija.s@live.fr>
2023-05-04 16:44:32 +0200

    game init commit

---
+++ game/main.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container');
+const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+function init(){
+    const newDiv = document.createElement('div');
+        
+}
+
